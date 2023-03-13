7b71a24b28f9c35a4e2804acb3e4fa58401f3a6a
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-13 11:55:05 -0400

    📦 NEW: New table created and Song object

--- app.py
+++ app.py
@@ -23,11 +23,18 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    album = db.Column(db.String(255), nullable=False)
+    relase_date = db.Column(db.Date, nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
 
-
+    def __repr__(self) -> str:
+        return f'{self.id} {self.title} {self.artist} {self.album} {self.relase_date} {self.genre}'
 
 # Schemas
-
 
 
 # Resources
